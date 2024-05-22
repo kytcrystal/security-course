d82b71a2577b4b8005a247dd0aac34b21a001245
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-05-22 21:42:59 +0200

    feat: blockchain class

---
+++ blockchain-for-security/blockchain-app.py
@@ -0,0 +1,14 @@
+import hashlib
+import json
+from time import time
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.nodes = set()
+        
+        # Create first block aka genesis block
+        self.create_block(proof=1, previous_hash=1)
+        
+    def create_block(self, proof, previous_hash):
+        pass
