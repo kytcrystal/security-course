0e6afc65ddd12f02085cd32a376ec0a98855669d
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-06-07 22:08:46 +0200

    feat: proof of work

--- blockchain-for-security/blockchain-app.py
+++ blockchain-for-security/blockchain-app.py
@@ -40,3 +40,15 @@ class Blockchain:
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+    
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
