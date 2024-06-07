335e27aa7efc6974ebd01013287c11d5860465e8
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-06-07 22:10:36 +0200

    refactor

--- blockchain-for-security/blockchain-app.py
+++ blockchain-for-security/blockchain-app.py
@@ -41,14 +41,14 @@ class Blockchain:
         return hashlib.sha256(block_string).hexdigest()
 
 
-    def proof_of_work(self, last_proof):
+    def proof_of_work(self, prev_proof):
         proof = 0
-        while self.valid_proof(last_proof, proof) is False:
+        while not self.valid_proof(prev_proof, proof):
             proof += 1
         return proof
     
     @staticmethod
-    def valid_proof(last_proof, proof):
-        guess = f'{last_proof}{proof}'.encode()
+    def valid_proof(prev_proof, proof):
+        guess = f'{prev_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
